986dab71264aa6f0acceb43969fc262fe5e23c23
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-21 16:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-//your code here
-let dragElement = null;
-
-// Store the dragged element
-function dragStart(e) {
-  dragElement = e.target;
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+const images = document.querySelectorAll(".image");
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-
-// Allow drop by preventing default behavior
-function dragOver(e) {
+function allowDrop(e) {
   e.preventDefault();
 }
-
-// Swap content (images) on drop
 function drop(e) {
-  e.preventDefault();
-  const target = e.target;
-  target.appendChild(dragElement);
-  dragElement = "";
-  dragElement.appendChild(target);
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+
+  dragdrop(clone);
+
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
+
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+
+images.forEach(dragdrop);
