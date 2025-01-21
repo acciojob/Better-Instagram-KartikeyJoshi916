dd9e7d4bde843cf793b39953c35b705683196b7b
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-21 14:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let dragElement = null;
+
+// Store the dragged element
+function dragStart(e) {
+  dragElement = e.target;
+}
+
+// Allow drop by preventing default behavior
+function dragOver(e) {
+  e.preventDefault();
+}
+
+// Swap content (images) on drop
+function drop(e) {
+  e.preventDefault();
+  const target = e.target;
+  target.appendChild(dragElement);
+  dragElement = "";
+  dragElement.appendChild(target);
+}
